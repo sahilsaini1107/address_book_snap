59e2f874beb3b2eaf8e44fc903fa20a35143d54d
sahilsaini <sahilsaini110>
2023-02-02 01:41:42 +0530

    address-book-created

--- src/index.js
+++ src/index.js
@@ -1,18 +1,59 @@
 module.exports.onRpcRequest = async ({ origin, request }) => {
+  let state = await wallet.request({
+    method: 'snap_manageState',
+    params: ['get'],
+  });
+
+  if (!state) {
+    state = {book:[]};
+    // initialize state if empty and set default data
+    await wallet.request({
+      method: 'snap_manageState',
+      params: ['update', state],
+    });
+  }
+
   switch (request.method) {
-    case 'hello':
+    case 'storeAddress':
+      state.book.push({
+        name:request.params.nameToStore,
+        address:request.params.addressToStore
+      });
+      await wallet.request({
+        method: 'snap_manageState',
+        params: ['update', state],
+      });
       return wallet.request({
         method: 'snap_confirm',
         params: [
           {
             prompt: `Hello, ${origin}!`,
             description:
-              'This custom confirmation is just for display purposes.',
+              'The address has been saved to your address book',
             textAreaContent:
-              'But you can edit the snap source code to make it do something, if you want to!',
+              `Name: ${request.params.nameToStore}\n`+
+              `Address: ${request.params.addressToStore}\n`+
+              `Addresses in book: ${state.book.length}`,
           },
         ],
       });
+    case 'retrieveAddresses':
+      return state.book;
+    case 'hello':
+      let address_book = state.book.map(function(item){
+        return `${item.name}: ${item.address}`;
+        })
+        .join('\n');
+    return wallet.request({
+      method: 'snap_confirm',
+      params: [
+        {
+          prompt: `Hello, ${origin}!`,
+          description: 'Address book:',
+          textAreaContent: address_book,
+        },
+      ],
+    });
     default:
       throw new Error('Method not found.');
   }
